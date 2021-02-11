c5b73615c7eee91ec42bb7ea8e8336e594c8e194
shovere <shovere@ufl.edu>
2021-02-11 01:58:48 -0500

    dbdone

--- prepare.jl
+++ prepare.jl
@@ -1,34 +1,31 @@
-
-#important note, any packages must be added via the repl terminal, no fuckin clue why 
-#im asuming ya gotta do like a config file or some shit so that the thing will work without a bunch of bullshit 
 using Pkg
 using ZipFile
 using SQLite
-using Tables
+using CSV
+using DataFrames
 
 zipName = ARGS[1]
-print(zipName, '\n')
-
+dbName = ARGS[2]
 nameRgx = r"^yob.*\.txt$"
+dateRgx = r"[12][0-9]{3}"
+
+db = SQLite.DB(dbName)
+DBInterface.execute(db, "CREATE TABLE names (
+year INTEGER,
+name TEXT,
+sex TEXT,
+num INTEGER
+)")
 
 rd = ZipFile.Reader(zipName)
 for file in rd.files
     if match(nameRgx, file.name) !== nothing
-        println("Filename: $(file.name)")
+        date = match(dateRgx, file.name).match 
+        namesDF = CSV.File(file, header = ["name","sex","num"]) |> DataFrame
+        namesDF[!,:year] .= date
+        namesDF |> SQLite.load!(db, "names")
     end 
 end 
+
 close(rd)
-
-txtTocsv(file)
-    
-
-# db = SQLite.DB("Baby_Names.sqlite")
-
-# DBInterface.execute(db, "CREATE TABLE names (
-# year INTEGER,
-# name TEXT,
-# sex TEXT,
-# num INTEGER
-# )")
-# println(DBInterface.execute(db, "SELECT * FROM names"))
-# println(db)
+close(db)
